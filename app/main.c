305a45733d310edf42cdf2c468c276311f6f39a5
Dmytro <dim4ukt@gmail.com>
2025-02-11 21:27:14 +0100

    added type command

--- app/main.c
+++ app/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
+  system("clear");
 
   // Uncomment this block to pass the first stage
   while (1) {
@@ -16,12 +17,24 @@ int main() {
   input[strcspn(input, "\n")] = '\0';
   if(strstr(input, "exit 0") != NULL){
     break;
-  }
-  if(strncmp(input, "echo", 4) == 0){
+  }else if(strncmp(input, "echo", 4) == 0){
     printf("%s\n", input + 5);
+  }else if(strncmp(input, "type", 4) == 0){
+
+    if (strncmp(input + 5, "echo", 4) == 0){
+      printf("echo is a shell builtin\n");
+    }else if(strncmp(input + 5, "type", 4) == 0){
+      printf("type is a shell builtin\n");
+    }else if(strncmp(input + 5, "exit", 4) == 0){
+      printf("exit is a shell builtin\n");
+    }else{
+      printf("%s: command not found\n", input + 5);
+    }
+
   }else{
   printf("%s: command not found\n", input);
   }
+
   }
   return 0;
 }
